6ed34d367023df4892fdd31d0f8bd8a6b5b57913
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2025-07-13 01:07:51 +0700

    Feat: Implement simple async FIFO behaviour

--- DSMessageBroker.App/Program.cs
+++ DSMessageBroker.App/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Concurrent;
+
+class SingleThreadTaskScheduler : TaskScheduler, IDisposable
+{
+    private readonly BlockingCollection<Task> _tasks = new();
+    private readonly Thread _thread;
+
+    public SingleThreadTaskScheduler()
+    {
+        _thread = new Thread(Run) { IsBackground = true };
+        _thread.Start();
+    }
+
+    private void Run()
+    {
+        foreach (var task in _tasks.GetConsumingEnumerable())
+        {
+            TryExecuteTask(task);
+        }
+    }
+
+    protected override IEnumerable<Task> GetScheduledTasks() => _tasks.ToArray();
+
+    protected override void QueueTask(Task task) => _tasks.Add(task);
+
+    protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) => false;
+
+    public void Dispose() => _tasks.CompleteAdding();
+}
---
+++ DSMessageBroker.Tests/MessageQueueTests.cs
@@ -0,0 +1,40 @@
+﻿using DSMessageBroker.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSMessageBroker.Tests
+{
+    public class MessageQueueTests
+    {
+        [Fact]
+        public async Task EnqueueDequeue_ShouldWorkInOrder()
+        {
+            var queue = new MessageQueue();
+            var msg1 = new Message { Id = Guid.NewGuid(), Body = "First", TimeStamp = DateTime.UtcNow };
+            var msg2 = new Message { Id = Guid.NewGuid(), Body = "Second", TimeStamp = DateTime.UtcNow };
+
+            await queue.EnqueueAsync(msg1);
+            await queue.EnqueueAsync(msg2);
+
+            var received1 = await queue.DequeueAsync();
+            var received2 = await queue.DequeueAsync();
+
+            Assert.Equal(msg1.Body, received1.Body);
+            Assert.Equal(msg2.Body, received2.Body);
+        }
+
+        [Fact]
+        public async Task Dequeue_Waits_WhenEmpty()
+        {
+            var queue = new MessageQueue();
+
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(2));
+            var dequeueTask = queue.DequeueAsync(cts.Token);
+
+            await Assert.ThrowsAsync<OperationCanceledException>(() => dequeueTask);
+        }
+    }
+}
